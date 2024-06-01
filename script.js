78ed210f16b85464eb35fba7fc46f21799f5f037
Ramya Moturi <82641085+ramyam29@users.noreply.github.com>
2024-06-01 21:53:52 +0530

    Update script.js
    added new feature to show same quote throughout the day

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const QuoteEle = document.getElementById("quote");
 
-fetch("quotes.json")
+fetch("quotes_clone.json")
   .then((response) => response.json())
   .then((data) => {
     const quotes = data;
@@ -10,17 +10,28 @@ fetch("quotes.json")
 
 function displayQuote(quotes) {
   let lastIndex = localStorage.getItem('lastQuoteIndex')
-  lastIndex = lastIndex ? parseInt(lastIndex) : -1;
-  const nextIndex = (lastIndex+1) % quotes.length;
-  const dailyQuote = quotes[nextIndex].quote;
+  let storedDate = localStorage.getItem('quoteDate');
+  const today = new Date().toISOString().split('T')[0];
+  console.log(today,storedDate);
+  if (storedDate === today && lastIndex !== null) {
+    // Use the stored quote index if the date is the same
+    const dailyQuote = quotes[parseInt(lastIndex)].quote;
+    QuoteEle.textContent = dailyQuote;
+    QuoteEle.classList.remove('loading');
+  } else {
+    // Update the quote if the date is different
+    lastIndex = lastIndex ? parseInt(lastIndex) : -1;
+    const nextIndex = (lastIndex + 1) % quotes.length;
+    const dailyQuote = quotes[nextIndex].quote;
   setTimeout(() => {
     QuoteEle.textContent = dailyQuote;
     QuoteEle.classList.remove('loading');
 }, 1000);
 
 localStorage.setItem('lastQuoteIndex',nextIndex)
+localStorage.setItem('quoteDate', today);
 }
-
+}
 
 //  modify displayQuote fn to display only one quote throughtout a day
 
