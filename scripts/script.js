475ad38d8ea949c9c901474257cb78668d11f21f
Venkat <srivenkatsairaj13@gmail.com>
2024-07-09 23:47:53 +0530

    save fav quotes

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,11 @@ import { copyQuote, showToast } from "./util.js";
 
 const setQuoteBtn = document.getElementById("set-quote");
 const randomQuoteBtn = document.getElementById("set-random");
-let quoteIndex;
+const likeBtn = document.getElementById("like-btn");
+const favoritesBtn = document.getElementById("list-fav");
+let lastQuoteIndex;
 let quotes;
+let currentIndex;
 
 /* for PWA implementation */
 if ("serviceWorker" in navigator) {
@@ -42,6 +45,9 @@ window.addEventListener("beforeinstallprompt", (e) => {
 
 /* Fetch and Display Quotes */
 const QuoteEle = document.getElementById("quote");
+const QuoteContainer = document.getElementsByClassName("quote-container")[0];
+const FavELe = document.getElementById("fav");
+const FavContainer = document.getElementsByClassName("fav-container")[0];
 
 fetch("backend/quotes.json")
   .then((response) => response.json())
@@ -57,19 +63,20 @@ function displayQuote(quotes) {
   let storedDate = localStorage.getItem("quoteDate");
 
   if (storedDate === today && lastIndex !== null) {
+    currentIndex = parseInt(lastIndex);
     QuoteEle.textContent = quotes[parseInt(lastIndex)].quote;
     QuoteEle.classList.remove("loading");
   } else {
     lastIndex = lastIndex ? parseInt(lastIndex) : -1;
-    const nextIndex = (lastIndex + 1) % quotes.length;
+    currentIndex = (lastIndex + 1) % quotes.length;
 
     setTimeout(() => {
-      QuoteEle.textContent = quotes[nextIndex].quote;
+      QuoteEle.textContent = quotes[currentIndex].quote;
       QuoteEle.classList.remove("loading");
       showConfetti();
     }, 1000);
 
-    localStorage.setItem("lastQuoteIndex", nextIndex);
+    localStorage.setItem("lastQuoteIndex", currentIndex);
     localStorage.setItem("quoteDate", today);
   }
 }
@@ -83,16 +90,41 @@ function showConfetti() {
 }
 
 function displayRandomQuote(quotes) {
-  let rnNum = Math.floor(Math.random() * quotes.length);
-  QuoteEle.textContent = quotes[rnNum].quote;
+  currentIndex = Math.floor(Math.random() * quotes.length);
+  QuoteEle.textContent = quotes[currentIndex].quote;
+}
+
+function saveFavQuoteIndex(index) {
+  let favoriteIndexes =
+    JSON.parse(localStorage.getItem("favoriteIndexes")) || [];
+  if (!favoriteIndexes.includes(index)) {
+    favoriteIndexes.push(index);
+    localStorage.setItem("favoriteIndexes", JSON.stringify(favoriteIndexes));
+  }
+}
+
+function displayFavoriteQuotes() {
+  const favoriteIndexes = JSON.parse(localStorage.getItem("favoriteIndexes"));
+  FavELe.innerHTML = "";
+  if (favoriteIndexes !== null) {
+    favoriteIndexes.forEach((value, index) => {
+      if (quotes && quotes[value]) {
+        const quote = quotes[value].quote;
+        FavELe.innerHTML += `<p>${index + 1} > ${quote}</p>`;
+      }
+    });
+    showToast(`You toggled favorites`);
+  } else {
+    showToast("No favorites added 😦");
+  }
 }
 
 setQuoteBtn.addEventListener("click", () => {
-  quoteIndex = prompt("Set localStorage ? Enter quote index");
-  if (quoteIndex !== null) {
-    showToast(`Displaying ${quoteIndex}th  quote 👀`);
-    localStorage.setItem("lastQuoteIndex", quoteIndex);
-    QuoteEle.textContent = quotes[quoteIndex].quote;
+  lastQuoteIndex = prompt("Set localStorage ? Enter quote index");
+  if (lastQuoteIndex !== null) {
+    showToast(`Displaying ${lastQuoteIndex}th  quote 👀`);
+    localStorage.setItem("lastQuoteIndex", lastQuoteIndex);
+    QuoteEle.textContent = quotes[lastQuoteIndex].quote;
   }
   return;
 });
@@ -100,4 +132,16 @@ setQuoteBtn.addEventListener("click", () => {
 randomQuoteBtn.addEventListener("click", () => {
   displayRandomQuote(quotes);
   showToast(`Random quote🔀`);
+});
+
+likeBtn.addEventListener("click", () => {
+  like.setAttribute("opacity", "1");
+  saveFavQuoteIndex(currentIndex);
+  showToast(`Saved to favorites❣️`);
+});
+
+favoritesBtn.addEventListener("click", () => {
+  displayFavoriteQuotes();
+  QuoteContainer.classList.toggle("hidden");
+  FavContainer.classList.toggle("hidden");
 });
